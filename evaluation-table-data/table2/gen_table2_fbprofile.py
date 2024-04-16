4bc46432039b292ee9ee25e1e6e79697747ca167
tracy <tracy@tracys-MBP.attlocal.net>
2024-04-16 00:39:26 -0400

    update table2 scripts

--- evaluation-table-data/table2/gen_table2_fbprofile.py
+++ evaluation-table-data/table2/gen_table2_fbprofile.py
@@ -5,23 +5,81 @@ import numpy as np
 import scipy.stats as st
 
 
+def getTimeList(fname):
 
-# display profile original
-time_data = [0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1] 
-time_data = [x + 50 for x in time_data]
+    logs = list()
+    count = 0
+    with open(fname, "r") as fp:
+        temp = list()
+        for line in fp.readlines():
+            temp.append(line)
+            count += 1
+            if count % 2 == 0:
+                logs.append(temp)
+                temp = list()
 
-avg = np.mean(time_data)
-(lo, hi) = st.t.interval(confidence=0.95, df=len(time_data)-1, loc=np.mean(time_data), scale=st.sem(time_data))
-print(avg, hi - avg, lo, hi)
-# (avg, hi) = st.t.interval(confidence=0.95, df=len(time_data)-1, loc=np.mean(time_data), scale=st.sem(time_data))
-# print(avg, hi - avg)
+    def getTime(line):
+        ret = line[:23]
 
-# display profile migrated
-time_data = [51, 51, 46, 41, 41, 39, 52, 44, 36, 40, 56, 38, 37, 41, 41, 37, 29, 46] 
-time_data = [x + 50 for x in time_data]
-avg = np.mean(time_data)
-(lo, hi) = st.t.interval(confidence=0.95, df=len(time_data)-1, loc=np.mean(time_data), scale=st.sem(time_data))
-print(avg, hi - avg, lo, hi)
+        dateTime = parser.parse(ret)
+        return dateTime
 
-# (avg, hi) = st.t.interval(confidence=0.95, df=len(time_data)-1, loc=np.mean(time_data), scale=st.sem(time_data))
-# print(avg, hi - avg)
+    l = []
+
+    for two_line in logs:
+        two_time = [getTime(l) for l in two_line]
+
+        delta1 = two_time[1] - two_time[0]
+
+        l.append(delta1.microseconds / 1000)
+        
+    
+    return l
+
+def getPESPTimeList(fname):
+
+    logs = list()
+    count = 0
+    with open(fname, "r") as fp:
+        temp = list()
+        for line in fp.readlines():
+            temp.append(line)
+            count += 1
+            if count % 2 == 0:
+                logs.append(temp)
+                temp = list()
+
+    def getPkgname(line):
+        ret = line[36:70]
+
+        return ret
+
+    def getTime(line):
+        ret = line[:23]
+
+        dateTime = parser.parse(ret)
+        return dateTime
+
+    l = []
+    for two_line in logs:
+        two_time = [getTime(l) for l in two_line]
+        two_pkgname = [getPkgname(l) for l in two_line]
+
+        delta1 = two_time[1] - two_time[0]
+ 
+        l.append(delta1.microseconds / 1000)
+    
+    return l
+
+
+baselinel = getTimeList("baseline_fbprofile_log.txt")
+pespl = getPESPTimeList("pesp_fbprofile_log.txt")
+
+def computeInterval(input_list):
+    avg = np.mean(input_list)
+    (lo, hi) = st.t.interval(confidence=0.95, df=len(input_list)-1, loc=np.mean(input_list), scale=st.sem(input_list))
+    print(avg, hi - avg, lo, hi)
+
+
+computeInterval(baselinel)
+computeInterval([a + b for (a, b) in zip(baselinel, pespl)])
--- evaluation-table-data/table2/gen_table2_twitter.py
+++ evaluation-table-data/table2/gen_table2_twitter.py
@@ -1,17 +1,95 @@
-twitter_original = [188,169,167,162,162,174,166,158,165,164]
-twitter_delta = [60, 67, 51, 111,50, 41, 50, 103, 43, 50]
-twitter_modified = [x + y for x, y in zip(twitter_original, twitter_delta)]
-# print(len(twitter_original))
-# print(len(twitter_delta))
-# print(twitter_modified)
-
-
+from time import time
+from dateutil import parser
+from datetime import timedelta
 import numpy as np
 import scipy.stats as st
-avg = np.mean(twitter_original)
-(lo, hi) = st.t.interval(confidence=0.95, df=len(twitter_original)-1, loc=np.mean(twitter_original), scale=st.sem(twitter_original))
-print(avg, hi - avg)
 
-avg = np.mean(twitter_modified)
-(lo, hi) = st.t.interval(confidence=0.95, df=len(twitter_modified)-1, loc=np.mean(twitter_modified), scale=st.sem(twitter_modified))
-print(avg, hi - avg)
+
+def getTimeList(fname):
+
+    logs = list()
+    count = 0
+    with open(fname, "r") as fp:
+        temp = list()
+        for line in fp.readlines():
+            temp.append(line)
+            count += 1
+            if count % 3 == 0:
+                logs.append(temp)
+                temp = list()
+
+    def getTime(line):
+        ret = line[:23]
+
+        dateTime = parser.parse(ret)
+        return dateTime
+
+    time_data = list()
+
+    l = []
+    for three_line in logs:
+        three_line = [getTime(l) for l in three_line]
+        # network_time = int(four_line[1].split("response time:")[1])
+        # print("network time: ", network_time)
+
+
+        delta1 = three_line[2] - three_line[0]
+
+   
+        delta2 = delta1.microseconds / 1000
+
+
+        l.append(delta2)
+        
+        
+    
+    return l
+
+def getPESPTimeList(fname):
+
+    logs = list()
+    count = 0
+    with open(fname, "r") as fp:
+        temp = list()
+        for line in fp.readlines():
+            temp.append(line)
+            count += 1
+            if count % 2 == 0:
+                logs.append(temp)
+                temp = list()
+
+    def getPkgname(line):
+        ret = line[36:70]
+
+        return ret
+
+    def getTime(line):
+        ret = line[:23]
+
+        dateTime = parser.parse(ret)
+        return dateTime
+
+    l = []
+
+    for two_line in logs:
+        two_time = [getTime(l) for l in two_line]
+        two_pkgname = [getPkgname(l) for l in two_line]
+
+        delta1 = two_time[1] - two_time[0]
+        l.append(delta1.microseconds / 1000)
+    
+    return l
+
+
+baselinel = getTimeList("baseline_twlogin_log.txt")
+pespl = getPESPTimeList("pesp_twlogin_log.txt")
+
+def computeInterval(input_list):
+    avg = np.mean(input_list)
+    (lo, hi) = st.t.interval(confidence=0.95, df=len(input_list)-1, loc=np.mean(input_list), scale=st.sem(input_list))
+    print(avg, hi - avg, lo, hi)
+
+
+computeInterval(baselinel)
+
+computeInterval([a + b for (a, b) in zip(baselinel, pespl)])
